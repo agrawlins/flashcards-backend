290595874b1052cb3995af0d31dfd60eea01fe99
agrawlins <agrawlins@gmail.com>
2021-08-09 22:49:48 -0400

    added a delete feature to products.js

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,24 @@ const {Product, validate} = require('../models/product');
 const express = require('express');
 const router = express.Router();
 
-//All endpoints and route handlers go here
+router.post('/', async (req, res) => {
+    try{
+        const{error} = validate(req.body);
+        if(error)
+            return res.status(400).send(error);
+        const product = new Product({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+        });
+        await product.save();
+        return res.send(product);
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 router.get('/', async (req, res) => {
     try{
         const products = await Product.find();
@@ -50,22 +67,16 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try{
-        const{error} = validate(req.body);
-        if(error)
-            return res.status(400).send(error);
-        const product = new Product({
-            name: req.body.name,
-            description: req.body.description,
-            category: req.body.category,
-            price: req.body.price,
-        });
-        await product.save();
+        const product = await Product.findByIdAndRemove(req.params.id);
+        if(!product)
+        return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
         return res.send(product);
-    }catch(ex){
+    } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+
 
 module.exports = router;
