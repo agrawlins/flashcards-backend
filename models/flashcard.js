38b0402cef7dd3e7dc0d6b7c15a2be513e7a6364
agrawlins <agrawlins@gmail.com>
2021-08-11 22:03:54 -0400

    changed name requirements to a number, instead of a string

--- models/flashcard.js
+++ models/flashcard.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const flashcardSchema = new mongoose.Schema({
-    name: {type: String, required: true, minlength: 1, maxlength: 255},
+    name: {type: Number, required: true},
     question: {type: String, required: true},
     category: {type: String, required: true, minlength: 5, maxlength: 50},
-    answer: {type: String, required: true, minlength: 2, maxlength: 255},
-    explanation: {type: String, required: true, minlength: 10, maxlength: 255},
+    answer: {type: String, required: true, minlength: 2, maxlength: 500},
+    explanation: {type: String, required: true, minlength: 10, maxlength: 500},
     dateModified: {type: Date, default: Date.now},
 });
 
@@ -14,11 +14,11 @@ const Flashcard = mongoose.model('Flashcard', flashcardSchema);
 
 function validateFlashcard(flashcard){
     const schema = Joi.object({
-        name: Joi.string().min(1).max(50).required(),
-        question: Joi.string().min(5).max(50).required(),
+        name: Joi.number().required(),
+        question: Joi.string().min(5).max(500).required(),
         category: Joi.string().min(5).max(50).required(),
-        answer: Joi.string().min(1).max(150).required(),
-        explanation: Joi.string().min(5).max(50).required(),
+        answer: Joi.string().min(1).max(500).required(),
+        explanation: Joi.string().min(5).max(500).required(),
     });
     return schema.validate(flashcard);
 }
